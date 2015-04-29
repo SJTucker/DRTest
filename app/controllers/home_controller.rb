2e957145c59368f6f4648d5008172a3387071cc2
Sam Tucker <samueljeffersontucker@gmail.com>
2015-04-29 18:38:32 -0500

    Update home_controller to add strong params and create method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,24 @@
 class HomeController < ApplicationController
 	def index
+		@rule = Rule.new
+		@rules = Rule.all
+	end
 
+	def create
+		@rule = Rule.new(rule_params)
+		if @rule.save
+			flash[:notice] = "Rule created"
+			redirect_to root_path
+		else
+			flash[:alert] = "Rule could not be created"
+			@rules = Rule.all
+			render :index
+		end
+	end
+
+	private
+
+	def rule_params
+		params.require(:rule).permit(:description, :cost_modifier)
 	end
 end
