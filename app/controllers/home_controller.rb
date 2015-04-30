d00fd1357a69900380257c058f074429f98a6447
Sam Tucker <samueljeffersontucker@gmail.com>
2015-04-30 13:04:37 -0500

    This is a javascript exercise, not a rails exercise ya dingus

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,25 +1,4 @@
 class HomeController < ApplicationController
 	def index
-		@rule = Rule.new
-		@rules = Rule.all
-	end
-
-	def create
-		@rule = Rule.new(rule_params)
-		if @rule.save
-			flash[:notice] = "Rule created"
-			redirect_to root_path
-		else
-			flash[:alert] = "Rule could not be created"
-			#HACK: this is kinda dumb, but it was easy to pass the instance variable back this way
-			@rules = Rule.all
-			render :index
-		end
-	end
-
-	private
-
-	def rule_params
-		params.require(:rule).permit(:description, :cost_modifier)
 	end
 end
