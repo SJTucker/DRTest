11c7eebaa3c29aec12ad0eb25c00b28091bf5e48
Sam Tucker <samueljeffersontucker@gmail.com>
2015-04-30 19:23:02 -0500

    Refactor to follow constraints concerning indirect adjustedwage manipulation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,28 +3,106 @@
 $(document).ready(init);
 
 function init() {
+	$('#add-rule').click(addRule);
 	$('#change-base-wage').click(changeBaseWage);
+	$('.modifier').click(modify);
+	$('#adjust-wage').click(adjustWage);
+	$('.rule-options').change(changeRule);
 };
 
+var modifier_function = increaseWage;
 var baseWage = 0;
+var adjustedWage = 0;
 var rules = [];
+var rule = {};
 
 function changeBaseWage() {
-	if(!rules.length){
-		baseWage = $('#base-wage-input').val()*1;
+	baseWage = $('#base-wage-input').val()*1;
 
-		//HACK: when a non-number is input, not sure why baseWage*1 is NaN and jQuery.type(baseWage*1) returns number, so I've got to do something silly.
-		//EDIT: Man I learned a bit about NaN tonight. NaN === NaN returns false. Do not like.
-		if(jQuery.type(baseWage) === 'number' && !isNaN(baseWage)) {
-			$('#base-wage-display').text('$' + baseWage + '/hr');
-		}else{
-			$('#alert').text('Rule could not be created.  Not a number.')
-		}
-	}else{
+	if(jQuery.type(baseWage) === 'number' && !isNaN(baseWage) && !rules.length) {
+		$('#base-wage-display').text('$' + baseWage + '/hr');
+	}else if(jQuery.type(baseWage) === 'number' && !isNaN(baseWage) && rules.length){
+		$('#base-wage-display').text('$' + baseWage + '/hr');
 		recalculateBaseWage();
+  }else{
+		$('#alert').text('Rule could not be created.  Not a number.')
 	}
 };
 
 function recalculateBaseWage() {
-
+	for (i = 0; i < rules.length; i++) {
+		adjustedWage = baseWage + rules[i];
+	}
 };
+
+function adjustWage() {
+	rule.modifier_function();
+	displayWage();
+};
+
+function modify() {
+	if($(this).attr('id') === 'decrease'){
+		modifier_function = decreaseWage;
+	}else{
+		modifier_function = increaseWage;
+	};
+};
+
+function addRule() {
+	rule.description = $('#description-input').val();
+	rule.price = $('#price-input').val()*1;
+	rule.modifier_function = modifier_function;
+
+	var modifier = '';
+	if(modifier_function === 'decreaseWage()'){
+		modifier = '-';
+	}
+
+	$('#rule-select').append('<option class="rule-options" value="' + rule + '">' + modifier + '$' + rule.price + ' ' + rule.description + '</option>');
+
+	rules.push(rule);
+};
+
+function increaseWage() {
+	if(adjustedWage === 0){
+		adjustedWage = baseWage + rule.price
+	}else{
+		adjustedWage += rule.price
+	}
+}
+
+function decreaseWage() {
+	if(adjustedWage === 0){
+		adjustedWage = baseWage - rule.price
+	}else{
+		adjustedWage -= rule.price
+	}
+}
+
+function changeRule() {
+	rule = this.val();
+}
+
+function displayWage() {
+	if(adjustedWage > 0){
+		$('#wage-display').text('$' + adjustedWage + '/hr');
+	}
+
+	if(adjustedWage < 0){
+		$('#alert').text('Cannot adjust wage.  Negative Number.')
+	}
+}
+
+/*if(adjustedWage === 0){
+		adjustedWage = (baseWage*1) + ((rule.modifier + rule.price)*1)
+	}else{
+		adjustedWage += ((rule.modifier + rule.price)*1)
+	}
+
+	if(adjustedWage > 0){
+		$('#wage-display').text('$' + adjustedWage + '/hr');
+	}
+
+	if(adjustedWage < 0){
+		$('#alert').text('Cannot adjust wage.  Negative Number.')
+	}*/
